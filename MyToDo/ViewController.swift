c6e25b43a60dcac230d1f8222e685675993d2d7b
Artem Sergeev <sergeev190391@gmail.com>
2024-01-16 15:17:58 +0200

    Initial Commit

---
+++ MyToDo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyToDo
+//
+//  Created by Артем Сергеев on 16.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+	}
+
+
+}
+
